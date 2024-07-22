f204208d4d73e378c276c49e6193542e47e25069
Isaiah Olaoye <Isaiah.Olaoye@optimaviz.com>
2024-07-22 10:27:25 +0100

    feat: preparing for server deployment

--- accounts/schema.py
+++ accounts/schema.py
@@ -1,9 +1,13 @@
+from typing import Type, Dict
+
 from ninja import Schema, ModelSchema
+from ninja_jwt.schema import TokenObtainInputSchemaBase
 
 from pydantic import EmailStr, field_validator
 
 from accounts.validator import validate_password, validate_email
 from accounts.models import User
+from core.helpers import get_tokens
 
 
 class UserInSchema(Schema):
@@ -13,21 +17,40 @@ class UserInSchema(Schema):
     mobile: str
     password: str
 
-    _validate_password = field_validator('password')(validate_password)
-    _validate_email = field_validator('email')(validate_email)
-
+    _validate_password = field_validator("password")(validate_password)
+    _validate_email = field_validator("email")(validate_email)
 
 
 class UserOutSchema(ModelSchema):
     class Meta:
         model = User
-        exclude = [
-            "groups",
-            "password",
-            "is_superuser",
-            "is_staff",
-            "user_permissions"
-        ]
+        exclude = ["groups", "password", "is_superuser", "is_staff", "user_permissions"]
+
+
+class CreateUserOutSchema(UserOutSchema):
+    token: dict
+
+    @staticmethod
+    def resolve_token(obj):
+        if not obj:
+            return
+        return get_tokens(obj)
+
+
+class MyTokenObtainPairOutSchema(Schema):
+    refresh: str
+    access: str
+
+
+class MyTokenObtainPairInputSchema(TokenObtainInputSchemaBase):
+    @classmethod
+    def get_response_schema(cls) -> Type[Schema]:
+        return MyTokenObtainPairOutSchema
+
+    @classmethod
+    def get_token(cls, user) -> Dict:
+        return get_tokens(user)  # type: ignore
+
 
 class ERROR_403OUTSCHEMA(Schema):
     error: str
--- kopmall/settings.py
+++ kopmall/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+from datetime import timedelta
+
+from decouple import config, Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pp33oe#cy2y&fihgq+&i_+e79n2tv^nak6o8s+s5s^$#b9(*sr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -71,10 +75,14 @@ AUTH_USER_MODEL = "accounts.User"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("NAME"),
+        "HOST": config("HOST"),
+        "USER": config("USER"), 
+        "PASSWORD": config("PASSWORD"), 
+        "PORT": config("PORT", cast=int),
+    },
 }
 
 
@@ -118,3 +126,47 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django project settings.py
+
+
+NINJA_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'ninja_jwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('ninja_jwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'ninja_jwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+
+    # For Controller Schemas
+    # FOR OBTAIN PAIR
+    'TOKEN_OBTAIN_PAIR_INPUT_SCHEMA': "accounts.schema.MyTokenObtainPairInputSchema",
+    'TOKEN_OBTAIN_PAIR_REFRESH_INPUT_SCHEMA': "ninja_jwt.schema.TokenRefreshInputSchema",
+    # FOR SLIDING TOKEN
+    'TOKEN_OBTAIN_SLIDING_INPUT_SCHEMA': "ninja_jwt.schema.TokenObtainSlidingInputSchema",
+    'TOKEN_OBTAIN_SLIDING_REFRESH_INPUT_SCHEMA':"ninja_jwt.schema.TokenRefreshSlidingInputSchema",
+
+    'TOKEN_BLACKLIST_INPUT_SCHEMA': "ninja_jwt.schema.TokenBlacklistInputSchema",
+    'TOKEN_VERIFY_INPUT_SCHEMA': "ninja_jwt.schema.TokenVerifyInputSchema",
+}
